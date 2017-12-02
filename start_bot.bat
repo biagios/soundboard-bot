c3c626966f1319540c716702fd9b8e58b19fef39
Redcone <31953477+Redcone@users.noreply.github.com>
2017-12-02 17:08:26 +0000

    .

--- start_bot.bat
+++ start_bot.bat
@@ -2,10 +2,10 @@ echo off
 cls
 echo -----
 echo /
-echo Please Ensure any other instances of Piney are closed.
+echo Please Ensure any other instances of Soundboard Bot are closed.
 echo Now hit any key to continue.
 echo \
 echo -----
 pause 
-start cmd /k node bot.js
+start cmd /k node app.js
 exit
