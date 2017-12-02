bbb888494c3b904dd7d111157b987da07c4249c1
sleme <16744664+sleme@users.noreply.github.com>
2017-12-02 17:52:41 +0000

    Updated more shit

--- app.js
+++ app.js
@@ -20,8 +20,8 @@ setInterval(function () {
   client.user.setGame(rangame)
 }, 60000 * 5)
 
-client.on('ready', () => {console.log("I  am ready also ben is a cunt"); console.log('Bot invite link: https://discordapp.com/oauth2/authorize?&client_id=' + config.client_id + '&scope=bot&permissions=1878522945')})
-client.on('disconnected', function () {console.log("I died wtf")})
+client.on('ready', () => {console.log("I  am ready."); console.log('Bot invite link: https://discordapp.com/oauth2/authorize?&client_id=' + config.client_id + '&scope=bot&permissions=1878522945')})
+client.on('disconnected', function () {console.log("Mission failed, I'll get them next time.")})
 client.on('reconnecting', () => console.log('I am reconnecting now!'))
 client.on('guildCreate', guild => {console.log("I joined a server")})
 client.on('guildDelete', guild => {console.log("I left a server")})
@@ -42,9 +42,29 @@ client.on('message', async message => {
     m.edit('It took ` ' + (m.createdTimestamp - message.createdTimestamp) + ' ms ` to ping ' + ranQuote + '\nAlso, the API latency is `' + Math.round(client.ping) + ' ms`')
   }
 
+  if (command === 'setgame') {
+    const game = args.join(' ')
+    message.delete().catch(O_o => {})
+    client.user.setGame(game)
+  }
+
   if (command === 'bot-invite') {
     message.delete()
     message.author.send('Bot invite link: https://discordapp.com/oauth2/authorize?&client_id=' + config.client_id + '&scope=bot&permissions=1878522945')
+  }
+  if (command === 'fuck'){
+    if (message.member.voiceChannel) {
+      message.member.voiceChannel.join()
+      .then(connection => {
+        const dispatcher = connection.playFile('/audio/airhorn.mp3');
+        dispatcher.on('end', () => {
+          message.member.voiceChannel.leave()
+        });
+      })
+      .catch(console.log);
+    } else {
+      message.reply('You need to join a voice channel first!');
+    }
   }
 })
 
